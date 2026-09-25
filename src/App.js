340a1a57ed32e47b7793740a6ffe86c2684776c2
Shirayaki <167289184+Shirayaki@users.noreply.github.com>
2026-09-25 15:36:26 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,13 +40,6 @@ export default function App() {
         {page === 'dash'   && <DashPage   entries={entries} kb={kb} />}
       </div>
       {toast && <Toast msg={toast} />}
-      <div className="app-container">
-      <Sidebar page={page} setPage={setPage} />
-      <main className="main-content">
-        {/* ページコンテンツ */}
-        {page === 'write' && <WritePage entries={entries} setEntries={setEntries} kb={kb} />}
-        {/* ... その他のページ ... */}
-      </main>
 
       {/* ← ここに FAB を追加 */}
       <div className="fab-button" onClick={() => setPage('write')}>
